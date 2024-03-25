5666f66d9e9a4ab092a19672d6d55ea0c5f376e6
john7319 <jonniekaras@gmail.com>
2024-03-25 15:32:20 +0300

    Assignment Complete(All tests passed)

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code your solution here
+function superbowlWin(element){
+    const win = element.find(yearFound => yearFound.result === "W")
+    return win ? win.year : undefined;
+}
+superbowlWin(record)
